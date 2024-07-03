ab8f566746c881e0b70e62c23baf8903e4843502
Emmanuel Iwok <iwokemmanuel49@gmail.com>
2024-07-04 00:00:27 +0100

    user deposit

--- routes/user/user.php
+++ routes/user/user.php
@@ -6,8 +6,5 @@ use App\Http\Controllers\user\UserController;
 
 // Index Pages
 Route::get('/user', [UserController::class, "user"])->name("user");
-<<<<<<< HEAD
 Route::get('/deposit', [UserController::class, "deposit"])->name("deposit");
 
-=======
->>>>>>> 69dd5d3dcee9b6111c1c44c08fbd93ee2f982444
