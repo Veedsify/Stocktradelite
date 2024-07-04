6905936fe11f5993eafe1dcff04b34ae24491aeb
dike <dikewisdom787@gmail.com>
2024-07-04 17:37:05 +0100

    added trading view widget

--- resources/views/layouts/user/userlayout.blade.php
+++ resources/views/layouts/user/userlayout.blade.php
@@ -1,43 +1,47 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr" data-bs-theme="light" data-color-theme="Blue_Theme" data-layout="vertical">
-  <head>
-    <!-- Required meta tags -->
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- Favicon icon-->
-<link rel="shortcut icon" type="image/png" href="{{ asset('user-assets/images/logos/favicon.png') }}">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+  <!-- Favicon icon-->
+  <link rel="shortcut icon" type="image/png" href="{{ asset('user-assets/images/logos/favicon.png') }}">
 
 
-<!-- Core Css -->
-<link rel="stylesheet" href="{{ asset('user-assets/css/styles.css') }}">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+    rel="stylesheet">
 
-    <title>Admins</title>
-    <!-- Owl Carousel  -->
-    <link rel="stylesheet" href="{{ asset('user-assets/libs/owl.carousel/dist/assets/owl.carousel.min.css') }}">
-  </head>
 
-  <body>
-    @yield('content')
-    <script src="{{ asset('user-assets/js/vendor.min.js') }}"></script>
-    <!-- Import Js Files -->
-<script src="{{ asset('user-assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('user-assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
-<script src="{{ asset('user-assets/js/theme/app.init.js') }}"></script>
-<script src="{{ asset('user-assets/js/theme/theme.js') }}"></script>
-<script src="{{ asset('user-assets/js/theme/app.min.js') }}"></script>
-<script src="{{ asset('user-assets/js/theme/sidebarmenu.js') }}"></script>
+  <!-- Core Css -->
+  <link rel="stylesheet" href="{{ asset('user-assets/css/styles.css') }}">
 
-<!-- solar icons -->
-<script src="{{ asset('npm/iconify-icon%401.0.8/dist/iconify-icon.min.js') }}"></script>
-    <script src="{{ asset('user-assets/libs/owl.carousel/dist/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('user-assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('user-assets/js/dashboards/dashboard.js') }}"></script>
-  </body>
+  <title>Admins</title>
+  <!-- Owl Carousel  -->
+  <link rel="stylesheet" href="{{ asset('user-assets/libs/owl.carousel/dist/assets/owl.carousel.min.css') }}">
+</head>
+
+<body>
+  @yield('content')
+
+  <script src="{{ asset('user-assets/js/vendor.min.js') }}"></script>
+  <!-- Import Js Files -->
+  <script src="{{ asset('user-assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('user-assets/libs/simplebar/dist/simplebar.min.js') }}"></script>
+  <script src="{{ asset('user-assets/js/theme/app.init.js') }}"></script>
+  <script src="{{ asset('user-assets/js/theme/theme.js') }}"></script>
+  <script src="{{ asset('user-assets/js/theme/app.min.js') }}"></script>
+  <script src="{{ asset('user-assets/js/theme/sidebarmenu.js') }}"></script>
+  <!-- solar icons -->
+  <script src="{{ asset('npm/iconify-icon%401.0.8/dist/iconify-icon.min.js') }}"></script>
+  <script src="{{ asset('user-assets/libs/owl.carousel/dist/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('user-assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+  <script src="{{ asset('user-assets/js/dashboards/dashboard.js') }}"></script>
+</body>
+
 </html>
