ea13c04cf2c94ad6752291c18aff421b6e699aa2
dike <dikewisdom787@gmail.com>
2024-07-08 01:01:32 +0100

    chore: Update Quill editor toolbar and mail form styling

--- public/user-assets/js/forms/quill-init.js
+++ public/user-assets/js/forms/quill-init.js
@@ -1,3 +1,43 @@
+function imageHandler() {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    input.click();
+
+    input.onchange = () => {
+        const file = input.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const base64ImageSrc = e.target.result;
+                const range = this.quill.getSelection();
+                this.quill.insertEmbed(range.index, 'image', base64ImageSrc);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+}
+
 var quill = new Quill("#editor", {
     theme: "snow",
+    modules: {
+        toolbar: {
+            container: [
+                [{
+                    font: []
+                }],
+                [{ header: [1, 2, 3, 4, 5, 6, false] }],
+                ["bold", "italic", "underline"],
+                [{ list: "ordered" }, { list: "bullet" }],
+                ["link"],
+                ["clean"],
+                ["code-block"],
+                ["blockquote"],
+                ["image", "video", "formula"]
+            ],
+            handlers: {
+                image: imageHandler
+            }
+        }
+    }
 });
--- resources/views/admin/mail.blade.php
+++ resources/views/admin/mail.blade.php
@@ -18,54 +18,51 @@
     <div class="body-wrapper">
       <div class="container-fluid">
         <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
-            <div class="card-body px-4 py-3">
-              <div class="row align-items-center">
-                <div class="col-9">
-                  <h4 class="fw-semibold mb-8">Send Mail</h4>
-                  <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      <li class="breadcrumb-item">
-                        <a class="text-muted text-decoration-none" href="index.html">Home</a>
-                      </li>
-                      <li class="breadcrumb-item" aria-current="page">mail</li>
-                    </ol>
-                  </nav>
-                </div>
-                <div class="col-3">
-                  <div class="text-center mb-n5">
-                    <img src="{{asset('user-assets/images/breadcrumb/ChatBc.png')}}" alt="" class="img-fluid mb-n4">
-                  </div>
+          <div class="card-body px-4 py-3">
+            <div class="row align-items-center">
+              <div class="col-9">
+                <h4 class="fw-semibold mb-8">Send Mail</h4>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                      <a class="text-muted text-decoration-none" href="index.html">Home</a>
+                    </li>
+                    <li class="breadcrumb-item" aria-current="page">mail</li>
+                  </ol>
+                </nav>
+              </div>
+              <div class="col-3">
+                <div class="text-center mb-n5">
+                  <img src="{{asset('user-assets/images/breadcrumb/ChatBc.png')}}" alt="" class="img-fluid mb-n4">
                 </div>
               </div>
             </div>
           </div>
+        </div>
 
-          <div class="card">
-            <div class="card-body">
-            
-              <form action="" class="form-horizontal">
-                <div class="mb-4">
-                  <label class="form-label">To:<span class="text-danger">*</span>
-                  </label>
-                  <input type="text" class="form-control"  >
+        <div class="card">
+          <div class="card-body">
+
+            <form action="" class="form-horizontal">
+              <div class="mb-4">
+                <label class="form-label">To:<span class="text-danger">*</span>
+                </label>
+                <input type="text" class="form-control">
+              </div>
+              <div class="mb-4">
+                <label class="form-label">Subject: <span class="text-danger">*</span>
+                </label>
+                <input type="text" class="form-control">
+              </div>
+              <div>
+                <label class="form-label">Message:</label>
+                <div id="editor" style="min-height: 30vh">
                 </div>
-                <div class="mb-4">
-                  <label class="form-label">Subject: <span class="text-danger">*</span>
-                  </label>
-                  <input type="text" class="form-control"  >
-                </div>
-                <div>
-                  <label class="form-label">Message:</label>
-                  <div id="editor" style="min-height: 30vh">
-                  </div>
-                </div>
-                <button class="btn btn-primary mt-2">Send</button>
-              </form>
-            </div>
+              </div>
+              <button class="btn btn-primary mt-2">Send</button>
+            </form>
           </div>
-
-
-
+        </div>
       </div>
     </div>
 
@@ -73,10 +70,6 @@
 
   </div>
   <x-admin.search />
-
-
-
-
 </div>
 
 @endsection
