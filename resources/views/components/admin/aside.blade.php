a7c1014d8a7cc934415782812ee52464c7cbb977
dike <dikewisdom787@gmail.com>
2024-07-16 02:18:40 +0100

    Properlly formatted code

--- resources/views/components/admin/aside.blade.php
+++ resources/views/components/admin/aside.blade.php
@@ -15,16 +15,10 @@
 
     <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
       <ul id="sidebarnav">
-        <!-- ---------------------------------- -->
-        <!-- Home -->
-        <!-- ---------------------------------- -->
         <li class="nav-small-cap">
           <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
           <span class="hide-menu">Main</span>
         </li>
-        <!-- ---------------------------------- -->
-        <!-- Dashboard -->
-        <!-- ---------------------------------- -->
         <li class="sidebar-item">
           <a class="sidebar-link" href="{{route('admin')}}" aria-expanded="false">
             <span>
